c33543bc6f4321a1c56a7da26eeb77ae1c2666e3
Jonus <48202867+chohyeonkeun@users.noreply.github.com>
2019-08-04 17:31:29 +0900

    Update example04_cgi.py

--- example04_cgi.py
+++ example04_cgi.py
@@ -3,7 +3,7 @@ import http.server
 PORT = 8003
 
 class Handler(http.server.CGIHTTPRequestHandler):
-    cgi_directories = ['/cgi']
+    cgi_directories = ['/cgi/']
 
 with http.server.HTTPServer(('127.0.0.1', PORT), Handler) as httpd:
     print('serving at port', PORT)
